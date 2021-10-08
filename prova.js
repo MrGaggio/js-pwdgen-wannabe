c10ceb68ea7f354a4fa5ffcfb87714d52abf2224
MrGaggio <steruggiu@gmail.com>
2021-10-08 15:41:15 +0100

    commit

--- prova.js
+++ prova.js
@@ -5,4 +5,4 @@ const userName = print("Come ti chiami?");
 const userSurname = print("Qual è il tuo cognome?");
 const userFavoriteColor = print("Qual è il tuo colore preferito?");
 
-console.log(userName);
+console.log(userName)
