844ad864ddce8d33de1ec4bba808af2d6a428e0f
MrGaggio <steruggiu@gmail.com>
2021-10-08 16:16:24 +0100

    prova stampa nome

--- prova.js
+++ prova.js
@@ -4,5 +4,9 @@
 const userName = prompt("Come ti chiami?");
 const userSurname = prompt("Qual è il tuo cognome?");
 const userFavoriteColor = prompt("Qual è il tuo colore preferito?");
+console.log(userName + userSurname + userFavoriteColor + 21);
 
-console.log(userName)
+
+//Scrivi sulla pagina nomecognomecolorepreferito21
+
+document.getElementById("nome").innerHTML= "userName" + "userSurname" + "userFavoriteColor";
