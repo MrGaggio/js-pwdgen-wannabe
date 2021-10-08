4bbf0f3255fb53839371f3efb26afece1e1f26e9
MrGaggio <steruggiu@gmail.com>
2021-10-08 12:40:02 +0100

    inserite le variabili nome, cognome e colore preferito

---
+++ file.js
@@ -0,0 +1,8 @@
+//Chiedi all'utente il suo nome, cognome e colore preferito
+
+
+const userName = print("Come ti chiami?");
+const userSurname = print("Qual è il tuo cognome?");
+const userFavoriteColor = print("Qual è il tuo colore preferito?");
+
+console.log(userName);
